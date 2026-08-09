56295db2fe72a50695d409a515b7c3b841361a59
Yuhui <yuhuicaowork@gmail.com>
2026-08-10 03:48:25 +1200

    Update diffusion_config.py

--- diffusion_config.py
+++ diffusion_config.py
@@ -14,6 +14,6 @@ class DiffusionConfig:
     beta_start: float = 1e-4
     beta_end: float = 0.02
     # Training: 
-    batch_size: int = 4
+    batch_size: int = 2
     epochs: int = 100
     learning_rate: float = 1e-4
