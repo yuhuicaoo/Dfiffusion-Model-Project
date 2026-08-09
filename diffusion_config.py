7830ac3217e76008fb723c5476681e74c3775c8c
Yuhui <yuhuicaowork@gmail.com>
2026-08-10 03:24:05 +1200

    Update diffusion_config.py

--- diffusion_config.py
+++ diffusion_config.py
@@ -5,7 +5,7 @@ import torch
 class DiffusionConfig:
     image_size: int = 256
     in_channels: int = 3        # number of color channels (1 = greyscale, 3 = RGB)
-    latent_channels: int = 4    # number of l
+    latent_channels: int = 4    # number of latent channels
     base_channels: int = 128     # network width (larger = smarter but slower)
     time_emb_dim: int = 256     # size of time signal vector
     timesteps: int = 1000       # how many steps until image becomes pure static noise
